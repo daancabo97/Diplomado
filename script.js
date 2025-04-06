a826c8f8bc0d9beb35f4d28e209f5cf8f68686f2
daancabo97 <danielcaicedo285@gmail.com>
2025-04-06 01:53:32 -0500

    PrimerProyecto

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-// FILTROS DE CATEGORÍA
 const botonesFiltro = document.querySelectorAll(".filtro");
 const itemsGaleria = document.querySelectorAll(".galeria-item");
 
-botonesFiltro.forEach(boton => {
+botonesFiltro.forEach((boton) => {
   boton.addEventListener("click", () => {
     const categoria = boton.getAttribute("data-categoria");
 
-    itemsGaleria.forEach(item => {
-      if (categoria === "todas" || item.classList.contains(categoria)) {
+    itemsGaleria.forEach((item) => {
+      if (categoria === "todas") {
         item.style.display = "block";
       } else {
-        item.style.display = "none";
+        if (item.classList.contains(categoria)) {
+          item.style.display = "block";
+        } else {
+          item.style.display = "none";
+        }
       }
     });
   });
